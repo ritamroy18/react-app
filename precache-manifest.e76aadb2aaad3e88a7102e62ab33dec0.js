2188605a316f10eedb72bea8e67a03c8949942ca
ritamroy18 <ritamroycs07@gmail.com>
2020-04-10 19:13:41 +0530

    Updates

---
+++ precache-manifest.e76aadb2aaad3e88a7102e62ab33dec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e671b2fc96e3a12963940f13f5af74b",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "d616360945d70a6fb467",
+    "url": "/hello-world/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bd29ae98787c6c7cbe17",
+    "url": "/hello-world/static/js/2.a2bf6d3e.chunk.js"
+  },
+  {
+    "revision": "d616360945d70a6fb467",
+    "url": "/hello-world/static/js/main.8b4fa373.chunk.js"
+  },
+  {
+    "revision": "52e180fc9346845ae5e2",
+    "url": "/hello-world/static/js/runtime-main.0a0d2884.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-world/static/media/logo.25bf045c.svg"
+  }
+]);
